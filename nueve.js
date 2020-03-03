77afe83c20923f972dff9abce17be1c0785c07e9
cintiacenturion <60634664+cintiacenturion@users.noreply.github.com>
2020-03-03 12:19:09 -0300

    Validacion

---
+++ nueve.js
@@ -0,0 +1,57 @@
+function mostrar()
+{
+
+var pasaje;
+var respuesta ="si";
+var edad;
+var sexo;
+var nombre;
+var nombreMiniHombreNacional;
+var primeraVezHombreNacional=true;
+
+      var respuesta ;
+      do {
+           do{
+               edad =(" ingrese edad");
+               edad =parseInt(edad);{
+
+               }while(isNaN(edad) || edad<18);
+               do{
+                   sexo =prompt("ingrese sexo");
+               }while (sexo !="m"&& sexo != "f");
+               do{
+                   pasaje =prompt("Ingrese pasaje");
+               }while(pasaje!="urbano" && pasaje !="nacional"&& pasaje !="internacional");
+//caluculos asegurarse que sea hombre y que el spasaje sea nacional
+if(sexo =="m"&& pasaje =="nacional"){
+
+// punto a 
+  if (primeraVezHombreNacional){
+        primeraVezNacional =false;
+        nombreMinimaHombreNacional =edad;
+        nombreMinimaHombreNacional =nombre;
+} else if(edad< edadMinimaHombreNacional){
+
+
+     
+    
+    }
+      repuesta =confirm("desea continuar?");
+
+{
+    while(respuesta);
+   
+
+
+ 
+  
+
+    
+    
+
+
+
+
+
+
+
