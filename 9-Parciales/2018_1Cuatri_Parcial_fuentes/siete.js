0f58c881609ea5d2738f0b607bd8f120c2c06bab
cintiacenturion <cent_cint@hotmail.com>
2020-03-05 16:26:20 -0300

    ejemplo parcial validacion sexo/nota

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,22 +1,65 @@
 function mostrar()
 {
-var sexo;
-var nota;
-var respuesta ="si";
-var promedioNotas;
-var notaMenor;
-var cantidadVarones;
+  var contador=0;
+  var nota;
+  var acumuladorSuma=0;
+  var promedio;
+  var miniNota;
+  var miniSexo;
+  var contadorVarones=0;
   
+  while(contador < 5){
+    contador ++;
+    do {
+      var nota = prompt("ingrese nota");
+      nota =parseInt(nota);
 
-   while (respuesta){
-     var nota = prompt("Debe Ingresar una nota?");
-     nota=parseInt(nota);
+    } while(isNaN(nota)|| nota < 0 || nota >10);
+    do{
+      var sexo =prompt("ingrese sexo");
+    }while(sexo!= "m" && sexo!="f");
+     // acumuladorSuma
+     acumuladorSuma += nota;
+   // punto b. minimo
+   if (contador ==1){
+     miniNota = nota;
 
-     }    
+   }else if (nota < miniNota){
+     miniNota =nota;
+     miniSexo = sexo;
 
-    
-
+   }
+   //punto c contador varones
+   if (nota >= 6 && sexo =="m"){
+     contadorVarones ++;
+   }
+  }
+  // calculo
+  promedio = acumuladorSuma /contador;
+  
+  alert ("Promedio "+ promedio);
+  alert ( "LA NOTA MAS BAJA ES:  " + miniNota+ " el alumno es de sexo " + sexo );
 
 
 
 }
+
+    
+      
+      
+     
+
+     
+       
+       
+
+
+
+
+
+    
+  
+
+  
+
+  
